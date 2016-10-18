24397bb9f7ff9cf96e20aa74003cd99c940e3283
JHades <jhades.dev@gmail.com>
2016-10-18 15:26:27 +0200

    ongoing REST API

--- hello-world.ts
+++ hello-world.ts
@@ -1,7 +1,4 @@
 
 
 
-
-
-
-
+console.log('Hello World !');
--- rest-examples.ts
+++ rest-examples.ts
@@ -1,5 +1,26 @@
 
-
-
 import * as express from 'express';
+import * as path from 'path';
+
+const port = 8090;
+
+const app = express();
+
+
+let root = path.join(path.resolve(__dirname, '.'));
+
+
+app.use(express.static(root));
+
+
+
+
+app.listen(port, () => {
+    console.log('Listen on port:' + port + ' at ' + new Date());
+});
+
+
+
+
+
 
