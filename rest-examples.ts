417dfaf0e7deb3d09d2bf404ea70abe3e0def56c
JHades <jhades.dev@gmail.com>
2016-10-18 16:29:56 +0200

    ongoing REST API

--- rest-examples.ts
+++ rest-examples.ts
@@ -1,6 +1,8 @@
 
 import * as express from 'express';
 import * as path from 'path';
+var bodyParser = require('body-parser');
+
 
 const port = 8090;
 
@@ -10,7 +12,21 @@ const app = express();
 let root = path.join(path.resolve(__dirname, '.'));
 
 
+app.use(bodyParser.json());
 app.use(express.static(root));
+
+
+function apiGetAllCourses(req: express.Request, res: express.Response) {
+
+    res.status(200).send();
+
+
+}
+
+
+app.route('/api/courses').get(apiGetAllCourses);
+
+
 
 
 
