fa9efba8f75a01b9963f9dd299b44460eb17ef4d
JHades <jhades.dev@gmail.com>
2016-10-19 17:14:07 +0200

    ongoing REST API

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { CourseComponent } from './course/course.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CourseComponent
   ],
   imports: [
     BrowserModule,
---
+++ frontend/src/app/course/course.component.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { CourseComponent } from './course.component';
+
+describe('Component: Course', () => {
+  it('should create an instance', () => {
+    let component = new CourseComponent();
+    expect(component).toBeTruthy();
+  });
+});
