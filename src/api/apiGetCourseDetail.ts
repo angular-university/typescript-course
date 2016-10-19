862084a8e82c2bc12303fcb71e56eff68cd5ffbc
JHades <jhades.dev@gmail.com>
2016-10-19 11:10:39 +0200

    ongoing REST API

--- src/api/apiGetCourseDetail.ts
+++ src/api/apiGetCourseDetail.ts
@@ -1,6 +1,17 @@
 
 import {Request, Response} from 'express';
+import {findCourseDetail} from "../queries/findCourseDetail";
+import {onSuccess} from "./onSuccess";
+import {onError} from "./onError";
+
+
 
 export function apiGetCourseDetail(req:Request, res:Response) {
+
+    const courseId = req.params.id;
+
+    findCourseDetail(courseId)
+        .then(_.partial(onSuccess, res))
+        .catch( _.partial(onError, res, `Could not find course detail for id ${courseId}`) );
 
 }
---
+++ src/queries/findCourseDetail.ts
@@ -0,0 +1,7 @@
+
+
+export function findCourseDetail(courseId:string) {
+
+    return Promise.resolve({});
+
+}
