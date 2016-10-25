ee8af9b2c68efbd18b0e903d8a392548de31d06d
Your Name <you@example.com>
2016-10-25 14:04:44 +0200

    typescript course ongoing

--- samples/learning-types.ts
+++ samples/learning-types.ts
@@ -1,12 +1,1 @@
 
-type ThreeNames = [string, string, string];
-
-let persons: ThreeNames = ['Kobe','Kareem', 'Shaq'];
-
-let counters: number[] = [0, 1,2];
-
-type PlayerTuple = [string, number];
-
-let tuple: PlayerTuple = ['Kobe', 5];
-
-
