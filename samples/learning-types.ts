0015051287c6d0e0d7d344947e8d4d84703cd2a1
Your Name <you@example.com>
2016-10-25 14:30:34 +0200

    typescript course ongoing

--- samples/learning-types.ts
+++ samples/learning-types.ts
@@ -1,1 +1,18 @@
 
+enum PlayerPosition {
+    Guard,
+    Forward,
+    Center
+}
+
+interface Player {
+    name:string;
+    position: PlayerPosition
+}
+
+let kobe: Player | null = {
+    name: 'Kobe',
+    position: PlayerPosition.Guard
+};
+
+kobe = null;
