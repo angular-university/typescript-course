fef6fe54f47a035be4c693317386d5d9f1066f35
Your Name <you@example.com>
2016-10-26 13:38:18 +0200

    typescript course ongoing

--- server/server.ts
+++ server/server.ts
@@ -1,6 +1,26 @@
 
+import * as ORM from "Sequelize";
+import {Sequelize} from 'Sequelize';
+
+const dbUrl = 'postgres://postgres:postgres@localhost:5432/complete-typescript-course';
+
+const sequelize:Sequelize = new ORM(dbUrl);
+
+const CourseModel =  sequelize.define('Course', {
+   description: ORM.STRING,
+    url:  ORM.STRING,
+    longDescription: ORM.TEXT,
+    iconUrl: ORM.STRING,
+    courseListIcon: ORM.STRING,
+    seqNo: ORM.INTEGER,
+    comingSoon: ORM.BOOLEAN,
+    isNew: ORM.BOOLEAN,
+    isOngoing: ORM.BOOLEAN
+});
 
 
+CourseModel.findAll()
+    .then(results => console.log(JSON.stringify(results)));
 
-console.log('Server is running...');
+
 
