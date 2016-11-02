2c324f0eb330eea7ae59154104c0a3872a2ef777
JHades <jhades.dev@gmail.com>
2016-11-02 16:48:20 +0100

    ongoing REST API

--- samples/generics.ts
+++ samples/generics.ts
@@ -1,3 +1,16 @@
 
 
-console.log('Generics');
+const heroes = ["Superman", "Batman", "Green Lantern", "Wonder Woman", "Flash"];
+
+
+function cloneArray(array:any[]): any[] {
+    return array.slice(0);
+}
+
+
+
+const clones = cloneArray(heroes);
+
+
+console.log("Printing clones", clones);
+
