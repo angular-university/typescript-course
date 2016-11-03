d2d064d3ca23299769570557c0c25fd3ade88cf1
Your Name <you@example.com>
2016-11-03 14:49:38 +0100

    typescript course ongoing

--- samples/decorators.ts
+++ samples/decorators.ts
@@ -1,16 +1,44 @@
 
+enum LoggingLevel {
+    INFO,
+    WARN,
+    DEBUG,
+    TRACE
+}
+
+const loggingLevel = LoggingLevel.INFO;
 
 
+function LogMethod(level: LoggingLevel):Function {
+    return (target: any, propertyKey: string,
+            descriptor: PropertyDescriptor) => {
+
+        const originalFunction:Function = descriptor.value;
+
+        descriptor.value = function(...args:any[]) {
+            if (level <= loggingLevel) {
+                console.log(">> " + propertyKey + " " +  JSON.stringify(args));
+            }
+            originalFunction.apply(this,args);
+        };
+
+
+    }
+
+}
 
 class Database {
 
-     name = 'CTCDB';
+    name = 'CTCDB';
 
+    @LogMethod(LoggingLevel.DEBUG)
     saveData(data:any) {
-        console.log('saving data in the database ' + this.name);
+        console.log(`saving data in the database ${this.name} ${JSON.stringify(data)}`);
     }
 
 }
 
 const db = new Database();
+
 db.saveData({message: 'Hello World !!'});
+
