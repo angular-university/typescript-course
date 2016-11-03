80d7942d1f537b9b6078479e781a0332edbb6b51
Your Name <you@example.com>
2016-11-03 12:06:28 +0100

    typescript course ongoing

--- samples/generics.ts
+++ samples/generics.ts
@@ -13,7 +13,7 @@ class Villain extends SuperCharacter {
 
 }
 
-class SuperTeam<T> {
+class SuperTeam<T extends SuperCharacter> {
     constructor(public members: T[], public leader: T) {
 
     }
@@ -27,11 +27,15 @@ const avengers = new SuperTeam([captainAmerica, thor, ironMan], captainAmerica);
 
 const members = avengers.members;
 
+const luthor = new Villain("Luthor");
+const bizarro = new Villain("Bizarro");
+const captainCold = new Villain("Captain Cold");
+
+const legionOfDoom = new SuperTeam([luthor, bizarro, captainCold], luthor);
 
 
-
-
-
+const megaCrossoverTeam = new SuperTeam([captainAmerica, thor, ironMan,
+    luthor, bizarro, captainCold], captainAmerica);
 
 
 
