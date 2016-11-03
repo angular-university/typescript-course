b005e87d6412f96a87c1c94b996fab5a55706ee0
Your Name <you@example.com>
2016-11-03 12:26:52 +0100

    typescript course ongoing

--- samples/decorators.ts
+++ samples/decorators.ts
@@ -1,2 +1,31 @@
 
+enum LoggingLevel {
+    INFO,
+    WARN,
+    DEBUG,
+    TRACE
+}
 
+const loggingLevel = LoggingLevel.DEBUG;
+
+function LogArguments(level: LoggingLevel) {
+    return (target: any, propertyKey: string, descriptor: PropertyDescriptor) => {
+        if (level >= loggingLevel) {
+            console.log(`>>> ${target.arguments}`);
+        }
+        return target;
+    }
+}
+
+
+class Database {
+
+    @LogArguments(LoggingLevel.DEBUG)
+    saveData(data:any) {
+        console.log('save data in the database ...');
+    }
+
+}
+
+const db = new Database();
+db.saveData({message: 'Hello World !!'});
