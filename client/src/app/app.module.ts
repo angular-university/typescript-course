ab00738002d22c9cf70e07086bf9aff329992f64
Your Name <you@example.com>
2016-11-07 15:59:56 +0100

    typescript course ongoing

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { CourseDetailComponent } from './course-detail/course-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CourseDetailComponent
   ],
   imports: [
     BrowserModule,
---
+++ client/src/app/course-detail/course-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'course-detail',
+  templateUrl: './course-detail.component.html',
+  styleUrls: ['./course-detail.component.css']
+})
+export class CourseDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
