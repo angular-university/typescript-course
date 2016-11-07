5640c09355cab8bac5aa07a2c0093f2b8fe1ac48
Your Name <you@example.com>
2016-11-07 12:44:33 +0100

    typescript course ongoing

--- server/queries/findAllCourses.ts
+++ server/queries/findAllCourses.ts
@@ -1,8 +1,9 @@
 
 
 import {CourseModel} from "../model/model";
+import {CourseSummary} from "../../shared/model/course-summary";
 
-export function findAllCourses() {
+export function findAllCourses() : Promise<CourseSummary[]>  {
     return CourseModel.findAll({
         order: ['seqNo']
     })
